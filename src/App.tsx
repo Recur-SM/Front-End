3de9d7ef599a860d10e66c9ae76f10393aef1892
현지현 <qwere4585@naver.com>
2026-02-04 02:00:09 +0900

    feat: 로그인 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
