9f3ee689b060be0dfbf742465d0e5ab654620c7e
Radian06 <aksen05aksen@gmail.com>
2026-02-06 18:39:48 +0900

    chore: 웹앱 환경 구축

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-import Tabbar from './components/tabbar';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/" element={<Tabbar />} />
     </Routes>
   );
 }
