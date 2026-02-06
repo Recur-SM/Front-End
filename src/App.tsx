1c323c1b1739fb3144a345095fe5ebfa9bb6c5d3
현지현 <qwere4585@naver.com>
2026-02-06 13:29:35 +0900

    feat: 메인페이지 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
