f10f952708ddcb0724c7e233ec7dd8d049a88387
Radian06 <aksen05aksen@gmail.com>
2026-02-07 00:28:20 +0900

    style: 웹앱 탭바 너비 조절

--- app/src/components/tabbar.tsx
+++ app/src/components/tabbar.tsx
@@ -9,7 +9,7 @@ const Tabbar: React.FC = () => {
     <div className="fixed bottom-0 w-[100vw] h-[6.87vh] px-[11.16vw] flex justify-between items-center rounded-t-[7.44vw] bg-white shadow-[0_-0.43vh_1.16vw_rgba(0,0,0,0.05)]">
         
         {/* 학습 관리 */}
-        <div className="w-[11.86vw] flex flex-col items-center">
+        <div className="w-[14vw] flex flex-col items-center">
             <img 
               src={BookIcon} 
               alt="학습 관리" 
@@ -19,7 +19,7 @@ const Tabbar: React.FC = () => {
         </div>
 
         {/* 과제 관리 */}
-        <div className="w-[11.86vw] flex flex-col items-center">
+        <div className="w-[14vw] flex flex-col items-center">
             <img 
               src={PencilIcon} 
               alt="과제 관리" 
@@ -29,7 +29,7 @@ const Tabbar: React.FC = () => {
         </div>
 
         {/* 마이페이지 */}
-        <div className="w-[11.86vw] flex flex-col items-center">
+        <div className="w-[14vw] flex flex-col items-center">
             <img 
               src={ProfileIcon} 
               alt="마이페이지" 
