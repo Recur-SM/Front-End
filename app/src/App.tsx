04ff5b2978b360aee3ce9f7aa68b23637ce00d12
Radian06 <aksen05aksen@gmail.com>
2026-02-06 19:54:44 +0900

    fix: 알 수 없는 오류 수정

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Tab from './components/tabbar';
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/tab" element={<Tab />} />
     </Routes>
   );
 }
