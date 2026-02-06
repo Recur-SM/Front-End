123d9f8e8ed08ccf4b2fa194f62e84eff6275149
Radian06 <aksen05aksen@gmail.com>
2026-02-06 14:00:33 +0900

    feat: 과제 관리 페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import Tapbar from './components/tapbar';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<Tapbar />} />
     </Routes>
   );
 }
