741fe58d7b11b8e1827082022d794706869f5f2b
Radian06 <aksen05aksen@gmail.com>
2026-02-06 23:41:00 +0900

    feat: 임시 코드 제거

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import Tab from './components/tabbar';
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/tab" element={<Tab />} />
     </Routes>
   );
 }
