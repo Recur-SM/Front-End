844b1e8d9b9e73f2a0ae815802bada589a347eda
Radian06 <aksen05aksen@gmail.com>
2026-02-06 15:24:16 +0900

    style: px->뷰포트 단위 변경

--- src/components/tabbar.tsx
+++ src/components/tabbar.tsx
@@ -1,21 +1,41 @@
+import React from 'react';
 import BookIcon from "../assets/bookIcon.svg";
 import PencilIcon from "../assets/pencilIcon.svg";
 import ProfileIcon from "../assets/profileIcon.svg";
 
 const Tabbar: React.FC = () => {
   return (
-    <div className="w-[334px] h-[64px] px-[48px] flex justify-between items-end rounded-t-[32px] bg-white">
-        <div className="mw-[51px] flex flex-col items-center">
-            <img src={BookIcon} alt="학습 관리" className="mw-[24.5px] h-[20.99px] mx-[1.75px] my-[3.5px] object-contain" ></img>
-            <div className="text-[12px] mt-[4px]">학습 관리</div>
+    /* 전체 바 */
+    <div className="w-[77.67vw] h-[6.87vh] px-[11.16vw] flex justify-between items-center rounded-t-[7.44vw] bg-white shadow-[0_-0.43vh_1.16vw_rgba(0,0,0,0.05)]">
+        
+        {/* 학습 관리 */}
+        <div className="w-[11.86vw] flex flex-col items-center">
+            <img 
+              src={BookIcon} 
+              alt="학습 관리" 
+              className="w-[5.7vw] h-[2.25vh] mx-[0.41vw] my-[0.38vh] object-contain" 
+            />
+            <div className="text-[2.79vw] mt-[0.43vh] font-medium text-[#999999]">학습 관리</div>
         </div>
-        <div className="mw-[51px] flex flex-col itmes-center">
-            <img src={PencilIcon} alt="과제 관리" className="mw-[16.16px] h-[17.58px] mx-[21.58px] my-[3.21px] object-contain" ></img>
-            <div className="text-[12px] mt-[5px] ml-[3px]">과제 관리</div>
+
+        {/* 과제 관리 */}
+        <div className="w-[11.86vw] flex flex-col items-center">
+            <img 
+              src={PencilIcon} 
+              alt="과제 관리" 
+              className="w-[3.76vw] h-[1.89vh] mx-[5.02vw] my-[0.34vh] object-contain" 
+            />
+            <div className="text-[2.79vw] mt-[0.54vh] ml-[0.7vw] font-medium text-[#999999]">과제 관리</div>
         </div>
-        <div className="mw-[51px] flex flex-col items-center">
-            <img src={ProfileIcon} alt="마이페이지" className="mw-[18.67px] h-[18.67px] m-[4.67px] object-contain" ></img>
-            <div className="text-[12px] mt-[4px]">마이페이지</div>
+
+        {/* 마이페이지 */}
+        <div className="w-[11.86vw] flex flex-col items-center">
+            <img 
+              src={ProfileIcon} 
+              alt="마이페이지" 
+              className="w-[4.34vw] h-[2.0vh] m-[1.09vw] object-contain" 
+            />
+            <div className="text-[2.79vw] mt-[0.43vh] font-medium text-[#999999]">마이페이지</div>
         </div>
     </div>
   );
