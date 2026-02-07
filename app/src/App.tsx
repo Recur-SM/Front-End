0932867ae282d3417d0e8dca44d50ebfb9e5bd00
Radian06 <aksen05aksen@gmail.com>
2026-02-07 17:09:46 +0900

    refactor: 웹앱 헤더&탭바 컴포 App.jsx로 이동

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // BrowserRouter 추가
 import HomePage from './pages/HomePage';
+import Header from "./components/header";
+import Tabbar from "./components/tabbar";
 
 function App() {
-
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <BrowserRouter>
+      <div className="relative min-h-screen flex flex-col items-center justify-between bg-[#F7F7F7]">
+        {/* 상단 헤더 컴포넌트 */}
+        <Header />
+
+        <main className="flex-1 w-full">
+           <Routes>
+             <Route path="/" element={<HomePage />} />
+           </Routes>
+        </main>
+
+        {/* 하단 탭바 컴포넌트 */}
+        <Tabbar />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,15 +1,9 @@
-import Header from "../components/header";
-import Tabbar from "../components/tabbar";
-
 const HomePage = () => {
   return (
-    <div className="relative flex flex-col items-center justify-between bg-[#F7F7F7]">
-      {/* 상단 헤더 컴포넌트 */}
-      <Header />
-
-      {/* 하단 탭바 컴포넌트 */}
-      <Tabbar />
-    </div>
+      <div className="w-[358x] h-[122px] p-[12px] rounded-[8px] bg-white">
+        <div className="text-[20px] text-semibold my-[1px]">오늘 할 일</div>
+        {/* 리스트 컴포넌트 */}
+      </div>
   );
 };
 
