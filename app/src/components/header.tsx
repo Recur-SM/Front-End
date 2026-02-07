b653e5c7b309bfc4bb13e58fbb21bb9df29862fc
Radian06 <aksen05aksen@gmail.com>
2026-02-07 22:00:18 +0900

    style: 상단 메뉴들 배치 수정

--- app/src/components/header.tsx
+++ app/src/components/header.tsx
@@ -3,18 +3,30 @@ import ProfileIcon from "../assets/profileIcon.svg";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full h-[172px] pt-[54px] flex justify-center bg-white rounded-b-[32px] shadow-sm z-50">
+    <header className="fixed top-0 left-0 w-full h-[172px] pt-[54px] flex justify-center bg-white rounded-b-[32px] shadow-[0px_4px_6px_0px_rgba(0,0,0,0.03)] z-50">
       <div className="w-full max-w-[430px] h-[56px] px-[24px] flex items-center justify-between">
-        <img src={MenuIcon} alt="메뉴" className="w-[20px] h-[20px] object-contain cursor-pointer" />
         
-        <div className="text-[20px] font-bold text-[#FF6738] flex-1 text-center">
-          설 스터디
+        <div className="flex-1 flex justify-start items-center">
+          <div className="w-[40px] h-[40px] flex items-center justify-start">
+            <img src={MenuIcon} alt="메뉴" className="w-[20px] h-[20px] object-contain cursor-pointer" />
+          </div>
         </div>
         
-        <div className="flex items-center gap-[6px] min-w-[70px] justify-end">
+        <div className="flex-none flex justify-center items-center">
+          <div className="text-[20px] font-bold text-[#FF6738] whitespace-nowrap">
+            설 스터디
+          </div>
+        </div>
+        
+        <div className="flex-1 flex justify-end items-center">
+          <div className="flex items-center gap-[6px] py-[4px]">
             <img src={ProfileIcon} alt="프로필" className="w-[18px] h-[18px]" />
-            <div className="text-[14px] font-medium whitespace-nowrap">김제현</div>
+            <div className="text-[14px] font-medium whitespace-nowrap text-[#333]">
+              김제현
+            </div>
+          </div>
         </div>
+
       </div>
     </header>
   );
