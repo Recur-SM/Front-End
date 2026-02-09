74ea1dc66c790e01e6b6da302c41c6e537a17a88
Radian06 <aksen05aksen@gmail.com>
2026-02-09 22:58:18 +0900

    refactor: 웹/웹앱 분리로 인한 경로 수정

--- src-app/components/sidebar.tsx
+++ src-app/components/sidebar.tsx
@@ -34,7 +34,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, userName }) => {
             </div>
           
             <button 
-                onClick={() => navigate('/mentee/assignment-detail')}
+                onClick={() => navigate('/mentee-a/assignment-detail')}
                 className="text-[12px] text-[#767676] mx-[8px] mb-[1.5px]"
                 >
                 마이페이지 &gt;
