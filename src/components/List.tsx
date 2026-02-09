0eda134d3cf18e18906735c075f946ff30a207ad
현지현 <qwere4585@naver.com>
2026-02-09 18:56:55 +0900

    feat: props에 tasks 추가 후 로직 수정

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,94 +1,36 @@
 import FilterButton from "./FilterButton";
 import { TABS, type TabType } from "../types/filter";
-import { useEffect, useState } from "react";
-import { LIST_TITLES, type FeedbackItem, type TodoItem } from "../types/list";
+import { useState } from "react";
+import { LIST_TITLES, type TodoItem } from "../types/list";
 import FilteredRow from "./learning/FilteredRow";
-import { getTasks } from "../api/task";
-import { useMenteeStore } from "../stores/menteeStroe";
-
-type FilterableItem = TodoItem | FeedbackItem;
 
 interface ListProps {
   title: string;
   type: "할일" | "피드백";
-  selectedDate?: string;
+  tasks: TodoItem[];
 }
 
-const List: React.FC<ListProps> = ({ title, type, selectedDate }) => {
+const List: React.FC<ListProps> = ({ title, type, tasks }) => {
   const [selectedTab, setSelectedTab] = useState<TabType>("전체");
-  const [todos, setTodos] = useState<TodoItem[]>([]);
-  // const [feedbacks, setFeedbacks] = useState<FeedbackItem[]>([]);
-  const { selectedMentee } = useMenteeStore();
-  const today = new Date().toISOString().split("T")[0];
-
-  const isFilterableType = (type: ListProps["type"]): type is "할일" | "피드백" => true;
-
-  useEffect(() => {
-    if (!selectedMentee) return; 
-    const fetchTasks = async () => {
-      try {
-        const res = await getTasks(selectedMentee.menteeId, today);
-
-        const fetchedTodos: TodoItem[] = res.result.tasks
-          .filter((t) => t.taskType === "ADDITIONAL")
-          .map((t) => ({
-            id: t.taskId,
-            title: t.taskName,
-            date: t.taskDate,
-            goal: t.taskGoal,
-            file: t.pdfFileUrl,
-             category: t.subjectName as TabType,
-            type: "할일",
-            isFeedback: t.hasFeedback,
-          }));
-
-        // const fetchedFeedbacks: FeedbackItem[] = res.result.tasks
-        //   .filter((t) => t.taskType === "FIXED") // 피드백 기준
-        //   .map((t) => ({
-        //     id: t.taskId,
-        //     title: t.taskName,
-        //     date: t.taskDate,
-        //     goal: t.taskGoal,
-        //     file: t.pdfFileUrl,
-        //     category: t.subjectName,
-        //     type: "피드백",
-        //     isFeedback: t.hasFeedback,
-        //   }));
-
-        setTodos(fetchedTodos);
-        // setFeedbacks(fetchedFeedbacks);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    fetchTasks();
-  }, [selectedMentee, today]);
-
-  const sourceItems = todos;
-
-  const filteredItems: FilterableItem[] = sourceItems.filter((item) =>
-    selectedDate ? item.date === selectedDate : true
-  ).filter((item) =>
-    selectedTab === "전체" ? true : item.category === selectedTab
+  const filteredTasks = tasks.filter((t) =>
+    selectedTab === "전체" ? true : t.category === selectedTab,
   );
 
   return (
     <div className="bg-white rounded-md px-8 py-5">
       <span className="flex text-lg font-semibold pb-3 px-1">{title}</span>
 
-      {isFilterableType(type) && (
-        <div className="flex gap-1 mb-3">
-          {TABS.map((tab) => (
-            <FilterButton
-              key={tab}
-              value={tab}
-              isActive={selectedTab === tab}
-              onClick={setSelectedTab}
-            />
-          ))}
-        </div>
-      )}
+      {/* 필터 버튼 */}
+      <div className="flex gap-1 mb-3">
+        {TABS.map((tab) => (
+          <FilterButton
+            key={tab}
+            value={tab}
+            isActive={selectedTab === tab}
+            onClick={setSelectedTab}
+          />
+        ))}
+      </div>
 
       <div className="grid grid-cols-5 gap-4 px-1 pt-2">
         {LIST_TITLES[type].map((col) => (
@@ -101,7 +43,7 @@ const List: React.FC<ListProps> = ({ title, type, selectedDate }) => {
       <div className="border-t border-gray-300 mb-4"></div>
 
       <div className="space-y-4 px-1">
-        {filteredItems.map((item) => (
+        {filteredTasks.map((item) => (
           <FilteredRow key={item.id} item={item} />
         ))}
       </div>
--- src/components/learning/StudyManagement.tsx
+++ src/components/learning/StudyManagement.tsx
@@ -1,12 +1,22 @@
+import type { TodoItem } from "../../types/list";
 import List from "../List";
 import CalendarWidget from "./calendar";
 
-const StudyManagement = () => {
+interface StudyManagementProps {
+  tasks: TodoItem[];
+}
+
+const StudyManagement: React.FC<StudyManagementProps> = ({
+  tasks,
+}) => {
+  const todoTasks = tasks.filter((t) => t.type === "할일");
+  // const feedbackTasks = tasks.filter((t) => t.type === "피드백");
+
   return (
     <div className="flex flex-col space-y-3">
-      <List title="오늘 할 일" type="할일" />
+      <List title="오늘 할 일" type="할일"  tasks={todoTasks} />
 
-      <List title="어제자 피드백" type="피드백" />
+      <List title="어제자 피드백" type="피드백"  tasks={todoTasks} />
 
       <div className="flex-col px-3 py-3">
         <div className="flex font-semibold text-lg">월간 계획표</div>
