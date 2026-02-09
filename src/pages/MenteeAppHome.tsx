2578663790c0fa8c0d6559e2f36152c85301b9d0
Radian06 <aksen05aksen@gmail.com>
2026-02-10 00:05:54 +0900

    feat: 웹앱 과제 관리 세부페이지 헤더 적용

--- src/pages/MenteeAppHome.tsx
+++ src/pages/MenteeAppHome.tsx
@@ -2,21 +2,30 @@ import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 
 import Header from "../../src-app/components/header";
+import DetailHeader from "../../src-app/components/detailHeader";
 import Tabbar from "../../src-app/components/tabbar";
 import Sidebar from "../../src-app/components/sidebar";
 import HomePage from '../../src-app/pages/HomePage';
 import AssignmentManagement from '../../src-app/pages/AssignmentManagement';
 import Record from '../../src-app/pages/Record';
+import AssignmentDetail from '../../src-app/pages/AssignmentDetail';
 
 function MenteeHome() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  
+  // 과제 세부 페이지 -> 헤더 변경
+  const isDetailPage = location.pathname === '/mentee-a/assignment-detail';
 
   return (
     <div className="relative min-h-screen bg-[#F7F7F7] w-full">
 
       {/* 상단 헤더 */}
       <div className="fixed top-0 z-50 w-full max-w-[430px]">
-        <Header onMenuClick={() => setIsSidebarOpen(true)} />
+        {isDetailPage ? (
+          <DetailHeader />
+        ) : (
+          <Header onMenuClick={() => setIsSidebarOpen(true)} /> // 기본 페이지일 때
+        )}
       </div>
 
       {/* 메인 콘텐츠 */}
@@ -25,6 +34,7 @@ function MenteeHome() {
           <Route path="/" element={<HomePage />} />
           <Route path="/assignment-management" element={<AssignmentManagement />} />
           <Route path="/record" element={<Record />} />
+          <Route path="/assignment-detail" element={<AssignmentDetail />} />
         </Routes>
       </main>
 
