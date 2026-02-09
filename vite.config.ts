0f39c1626d385cfe7535e7f3638a0b6fafcb8e8e
sumi-03 <lsmkoo03@naver.com>
2026-02-10 02:19:36 +0900

    chore: 앱 진입 및 빌드 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        app: resolve(__dirname, "app.html"),
+      },
+    },
+  },
   server: {
     proxy: {
-      '/api': {
-        target: 'https://back-end-production-4beb.up.railway.app', // 실제 백엔드 주소
+      "/api": {
+        target: "https://back-end-production-4beb.up.railway.app", // 실제 백엔드 주소
         changeOrigin: true,
         secure: true,
       },
