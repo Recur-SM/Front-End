88ee2e8e082333e8b4ba3be9a48c8c711b61b4e6
현지현 <qwere4585@naver.com>
2026-02-10 18:50:31 +0900

    feat: 플래너 코멘트 api 연동

--- src/components/assignment/FeedbackBoard.tsx
+++ src/components/assignment/FeedbackBoard.tsx
@@ -1,11 +1,48 @@
 import { useState } from "react";
+import type { AddPlannerCommentRequest } from "../../types/planner";
+import { addPlannerComment } from "../../api/planner";
 
 interface FeedbackBoardProps {
   height?: number;
+  plannerId: number;
+  menteeId: number;
+  mentorId: number;
 }
 
-const FeedbackBoard: React.FC<FeedbackBoardProps> = ({ height = 130 }) => {
+const FeedbackBoard: React.FC<FeedbackBoardProps> = ({
+  height = 130,
+  plannerId,
+  menteeId,
+  mentorId,
+}) => {
   const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    if (!value) return; // 내용 없으면 종료
+    setLoading(true);
+
+    try {
+      const payload: AddPlannerCommentRequest = {
+        plannerId,
+        menteeId,
+        mentorId,
+        mentorComment: value,
+      };
+
+      const response = await addPlannerComment(payload);
+      console.log("작성 완료:", response);
+
+      // 요청 성공 후 입력 초기화
+      setValue("");
+      alert("피드백이 저장되었습니다!");
+    } catch (err) {
+      console.error(err);
+      alert("피드백 저장에 실패했습니다.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div
@@ -32,6 +69,27 @@ const FeedbackBoard: React.FC<FeedbackBoardProps> = ({ height = 130 }) => {
             px-10
           "
       />
+
+      <button
+        onClick={handleSubmit}
+        disabled={loading}
+        className="
+            absolute
+            bottom-3
+            right-3
+            text-xs
+            px-3
+            py-1
+            border
+            rounded-2xl
+            bg-white
+            hover:bg-gray-50
+            disabled:opacity-50
+            disabled:cursor-not-allowed
+          "
+      >
+        {loading ? "저장 중..." : "작성 완료"}
+      </button>
     </div>
   );
 };
