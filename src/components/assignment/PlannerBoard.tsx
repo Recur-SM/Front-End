13c02c4d4cb9ca4f6ba61c8423cbef350d47c9a7
현지현 <qwere4585@naver.com>
2026-02-10 17:33:41 +0900

    feat: 플래너 조회 api 연동

--- src/components/assignment/PlannerBoard.tsx
+++ src/components/assignment/PlannerBoard.tsx
@@ -1,4 +1,26 @@
-const PlannerBoard = () => {
+import { useEffect } from "react";
+import { getPlanner } from "../../api/planner";
+
+interface PlannerProps {
+  menteeId: number;
+  date: string;
+}
+
+const PlannerBoard: React.FC<PlannerProps> = ({ menteeId, date }) => {
+  
+  useEffect(() => {
+    const fetchPlanner = async () => {
+      try {
+        const res = await getPlanner(menteeId, date);
+        console.log(res);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchPlanner();
+  }, [menteeId, date]);
+
   return (
     <div className="flex w-full justify-center items-center bg-[#99999933] h-[130px] rounded-lg border border-[#767676] text-sm">
       <span className="text-[#767676]">등록된 플래너가 없습니다</span>
